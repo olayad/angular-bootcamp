949204007eaea0600e973048b1b2077466a8c0bf
Daniel Olaya <d_olaya@hotmail.com>
2020-03-25 11:30:30 -0700

    Custom property binding

--- 02-cmp-databinding-start/src/app/app.component.ts
+++ 02-cmp-databinding-start/src/app/app.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
+
 
 
 
