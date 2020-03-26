ebd5646882a7ea75a786a6eb56ef5665ce2d87f4
Daniel Olaya <d_olaya@hotmail.com>
2020-03-26 15:43:25 -0700

    Lifecycle hooks intro

--- 02-cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ 02-cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,16 +1,53 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: {type: string, name: string, content: string};
+export class ServerElementComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked
+{
+  // @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('constructor called');
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log('changes', changes)
+  }
+
+
+  ngOnInit(): void {
+    console.log('ngOnInit called')
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck called')
+  }
+
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called!');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContenChecked called!');
+  }
 }
