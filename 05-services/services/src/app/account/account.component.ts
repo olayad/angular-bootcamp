9dbe38e1984c304764f0672afe8bcb649f1de36a
Daniel Olaya <d_olaya@hotmail.com>
2020-03-31 16:47:22 -0700

    Injectable intro

--- 05-services/services/src/app/account/account.component.ts
+++ 05-services/services/src/app/account/account.component.ts
@@ -6,21 +6,19 @@ import {AccountsService} from "../accounts.service";
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  // @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
   constructor(private loggingService: LoggingService,
               private accountsService: AccountsService){}
 
   onSetTo(status: string) {
     this.accountsService.updateStatus(this.id, status);
-    // this.statusChanged.emit({id: this.id, newStatus: status});
-    this.loggingService.logStatusChange(status);
+    // this.loggingService.logStatusChange(status);
 
   }
 }
