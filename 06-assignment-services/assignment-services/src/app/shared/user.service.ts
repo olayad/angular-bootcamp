db9ecc2ece680da447f3a4a6b9a5c83b3c74bfcc
Daniel Olaya <d_olaya@hotmail.com>
2020-04-01 11:57:47 -0700

    Added service counter

--- 06-assignment-services/assignment-services/src/app/shared/user.service.ts
+++ 06-assignment-services/assignment-services/src/app/shared/user.service.ts
@@ -5,18 +5,22 @@ export class UserService {
   statusUpdated = new EventEmitter<{currentStatus: string, id: number}>();
   private activeUsers = ['Mospi', 'Carlos'];
   private inactiveUsers = ['Teye', 'Vic'];
+  private activeToInactiveCtr = 0;
+  private inactiveToActiveCtr = 0;
 
   constructor(){
       this.statusUpdated.subscribe(
       (user) => {
         if (user.currentStatus === 'active') {
-          console.log('change to inactive', user.currentStatus, user.id);
           this.inactiveUsers.push(this.activeUsers[user.id]);
           this.activeUsers.splice(user.id, 1);
+          this.activeToInactiveCtr++;
+          console.log("change to inactive: " + user.currentStatus + " " + user.id + " ctr: " + this.activeToInactiveCtr);
         } else {
-          console.log('change to active', user.currentStatus, user.id);
           this.activeUsers.push(this.inactiveUsers[user.id]);
           this.inactiveUsers.splice(user.id, 1);
+          this.inactiveToActiveCtr++
+          console.log('change to active: ' + user.currentStatus + ' ' + user.id + ' ctr: ', this.inactiveToActiveCtr);
         }
         return true;
       }
